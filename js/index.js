fb40f7e6529784ddcee2bb86fd0d215e08131b08
Francisco Moreno <50809863+fmoredesigns@users.noreply.github.com>
2020-03-22 00:05:55 +0100

    Initialize

---
+++ js/index.js
@@ -0,0 +1,36 @@
+var companies =[{'name':'Apotek 1','category':'Helse og velvære','price':'99','free_delivery_limit':'','homepage':'apotek1.no','logo':'https://morenenkjopesenter.no/wp-content/uploads/2014/02/logo-apotek1.png'},{'name':'Vitusapotek','category':'Helse og velvære','price':'99','free_delivery_limit':'','homepage':'vitusapotek.no','logo':'https://images.squarespace-cdn.com/content/v1/5beb0ba8a2772c47a1392921/1543603980972-C2IE9HA4KR8AYPMMWEG5/ke17ZwdGBToddI8pDm48kL6ihLOnGesp-xL9jch5JeEUqsxRUqqbr1mOJYKfIPR7LoDQ9mXPOjoJoqy81S2I8N_N4V1vUb5AoIIIbLZhVYwL8IeDg6_3B-BRuF4nNrNcQkVuAT7tdErd0wQFEGFSnP5a5fnbVoLQRf69X016CfNJfbpQa87jnEIrMbWMh1cPd22pOepWSyIIsp8XMhdonw/Vitusapotek+logo.png'},{'name':'Lensway','category':'Helse og velvære','price':'50','free_delivery_limit':'','homepage':'lensway.no','logo':'https://res.cloudinary.com/lwg/image/upload/v1/static/lensway-logo.png'},{'name':'Plantasjen','category':'Interiør, gave og hobby','price':'99','free_delivery_limit':'','homepage':'plantasjen.no','logo':'https://kommunikasjon.ntb.no/data/images/00891/5529ba67-d1e1-4edf-b3e7-b7adfd657d98-w_960_h_960.png'},{'name':'Jernia','category':'Interiør, gave og hobby','price':'29','free_delivery_limit':'','homepage':'jernia.no','logo':'https://gunerius.no/globalassets/fellesgemensamt---webolavthon.no/-butikklogos/j/jernia2_logo.png'},{'name':'Jula','category':'Interiør, gave og hobby','price':'249','free_delivery_limit':'','homepage':'jula.no','logo':'https://www.jula.no/Frontend/Build/images/logo.png'},{'name':'Europris','category':'Interiør, gave og hobby','price':'39','free_delivery_limit':'','homepage':'europris.no','logo':'https://amfi.no/globalassets/fellesgemensamt---webolavthon.no/-butikklogos/e/europris-logo-500x300-px.png'},{'name':'Clas Ohlson','category':'Interiør, gave og hobby','price':'99','free_delivery_limit':'','homepage':'clasohlson.com/no/','logo':'https://amfi.no/globalassets/fellesgemensamt---webolavthon.no/-butikklogos/c/clas-ohlson-logo-500x300-px.png'},{'name':'Adlibris','category':'Bøker og mer','price':'42','free_delivery_limit':'','homepage':'adlibris.no','logo':'https://logovectorseek.com/wp-content/uploads/2019/11/adlibris-logo-vector.png'},{'name':'Ark','category':'Bøker og mer','price':'45','free_delivery_limit':'','homepage':'ark.no','logo':'https://amfi.no/globalassets/amfi-no/amfi-felles/logoer/butikklogoer/a/ark-logo-500x300-px.png'},{'name':'Kid','category':'Interiør, gave og hobby','price':'69','free_delivery_limit':'','homepage':'kid.no','logo':'https://ccvest.no/wp-content/uploads/2019/02/Kid-logo.png'},{'name':'Tilbords','category':'Interiør, gave og hobby','price':'79','free_delivery_limit':'','homepage':'tilbords.no','logo':'https://www.tilbords.no/Styles/gfx/logo-tilbords_w.png'},{'name':'Kitchn','category':'Interiør, gave og hobby','price':'79','free_delivery_limit':'','homepage':'kitchn.no','logo':'https://amfi.no/globalassets/amfi-no/amfi-felles/logoer/butikklogoer/k/kitchn-logo-500x300-px.png'},{'name':'Dressmann','category':'Klær, sko og tilbehør','price':'Gratis','free_delivery_limit':'','homepage':'dressmann.no','logo':'https://4.bp.blogspot.com/-f0Xr7ddXqSE/TWHT2FOKRkI/AAAAAAAAAQQ/eTM2CdZBuWU/s1600/Dressmann+logo+2011.png'},{'name':'Lindex','category':'Klær, sko og tilbehør','price':'39','free_delivery_limit':'249','homepage':'lindex.com/no','logo':'https://www.lindex.com/Assets/SiteV3/UI/logo.png'},{'name':'H&M','category':'Klær, sko og tilbehør','price':'79,9','free_delivery_limit':'300','homepage':'http://hm.com/no','logo':'https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/H%26M-Logo.svg/709px-H%26M-Logo.svg.png'},{'name':'Jack & Jones','category':'Klær, sko og tilbehør','price':'59','free_delivery_limit':'','homepage':'https://www.jackjones.com/no/','logo':'https://cdn.freelogovectors.net/wp-content/uploads/2020/02/jack-jones-logo.png'},{'name':'Meny','category':'Mat og drikke','price':'fra 59,00kr','free_delivery_limit':'','homepage':'meny.no','logo':'https://kommunikasjon.ntb.no/data/images/00999/4841e2e2-2bae-4bdc-84e7-385c1b7b0855-w_960_h_960.png'},{'name':'Kolonial','category':'Mat og drikke','price':'fra 0,00 kr','free_delivery_limit':'','homepage':'kolonial.no','logo':'http://matkasser.com/wp-content/uploads/2016/09/kolonialno-main.png'},{'name':'Morgenlevering','category':'Mat og drikke','price':'4,9','free_delivery_limit':'150','homepage':'morgenlevering.no','logo':'http://matkasser.com/wp-content/uploads/2016/09/Morgenlevering-300x300.png'},{'name':'Adams matkasse','category':'Matkasser','price':'0','free_delivery_limit':'','homepage':'adamsmatkasse.no','logo':'http://teknojobb.no/kunder/teknojobb/images/ad_6518_godtlevertadamsmatkasse.png'},{'name':'Godt levert','category':'Matkasser','price':'0','free_delivery_limit':'','homepage':'godtlevert.no','logo':'https://godtlevert.no/assets/icons/logo-full.svg'},{'name':'Helthjem','category':'Send og motta pakke','price':'fra 75,00kr','free_delivery_limit':'','homepage':'helthjem.no','logo':'https://s3-eu-west-1.amazonaws.com/tpd/logos/59d3a1240000ff0005acb1c5/0x0.png'},{'name':'wolt','category':'Take-away','price':'','free_delivery_limit':'','homepage':'https://wolt.com/','logo':'https://upload.wikimedia.org/wikipedia/commons/8/88/Wolt_logo_black.png'},{'name':'Foodora','category':'Take-away','price':'','free_delivery_limit':'','homepage':'https://www.foodora.no/','logo':'https://kroathai.com/wp-content/uploads/foodora-logo.png'}];
+
+function addCategory(category, category_id) {
+  $('.company-list').append('<p class="category-name">'+category+'</p>');
+  $('.company-list').append('<div class="category" id="category_'+category_id+'"></div>');
+  $('#category_'+category_id).append('<div class="companies"> </div>');
+}
+
+function addCompany(company, category_id, company_id) {
+  $('#'+category_id+ ' .companies').append('<div class="company" id="company_'+company_id+'"><div class="company-name">'+company.name+'</div><div class="company-specs"><div class="spec"><p class="spec-title">Levering pris fra</p><p class="price-from">'+company.price+'</p></div><div class="spec"><p class="spec-title">Frifrakt grense</p><p class="price-from">'+company.free_delivery_limit+'</p></div></div></div></div>');
+  //$('#company_'+company_id+' .company-logo').css("background-image", "url(" + company.logo + ")");
+}
+
+$(document).ready(function(){
+  let categories = [];
+
+  for (var i in companies){
+    let temp_category = companies[i].category;
+    if(categories.indexOf(temp_category) < 0){
+      categories.push(temp_category);
+    }
+  }
+
+  let id = 1;
+  for (var i in categories){
+    addCategory(categories[i], id);
+    id++;
+  }
+
+  let company_id = 1;
+  for (company of companies){
+    let category_id = 'category_'+ (categories.indexOf(company.category) + 1);
+    addCompany(company, category_id, company_id);
+    company_id ++;
+  }
+});
